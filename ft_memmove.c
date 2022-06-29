94cb6efeb50d3388bb733b56ad0b7969aae9eef4
Marcos Henrique <51037060+dragonforce725@users.noreply.github.com>
2022-06-29 15:50:26 -0300

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,12 +14,18 @@
 
 void	*ft_memmove(void *str1, const void *str2, size_t n)
 {
+	size_t c;
+
+	c = 0;
 	if (str1 == NULL && str2 == NULL)
 		return (NULL);
-	if (str1 < str2)
+	if ((size_t)str1 - (size_t)str2 < n)
 		ft_memcpy(str1, str2, n);
 	else
-		while (n--)
-			*(unsigned char *)(str1 + n) = *(unsigned char *)(str2 + n);
+		while (c < n)
+		{
+			((unsigned char *)str1)[c] = ((unsigned char *)str2)[c];
+			c++;
+		}
 	return (str1);
 }
